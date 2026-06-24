32fdf86698cbad48e960e645bbd8fb096b727ea8
itzelgreen <itzel@bravamgmt.com>
2026-06-24 14:04:05 -0400

    admin/config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+window.BRAVA_API_URL = "https://script.google.com/macros/s/AKfycbwJXKGq3T1qTGLCFbY5iH-OTpDJRSvhFccDzaJocF5a40Dn4Fj3WKsRVUgzfyTQHyv6/exec";
+window.BRAVA_VOUCHER_BASE_URL = "https://itzelgreen.github.io/brava-voucher-/voucher";
